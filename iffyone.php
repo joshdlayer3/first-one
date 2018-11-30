29f4f1b33122f6d9caa878062b97d8322bc2765a
Josh Douglas <joshd@layer3.co.uk>
2018-11-30 16:48:32 +0000

    thrid commit m88888

---
+++ iffyone.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joshdouglas
+ * Date: 2018-11-30
+ * Time: 15:37
+ */
+$christ = 99;
+if ($christ===100){
+    echo "<h1>god help us all<h1/>";
+} else {
+    echo "<h2>there is no hope if man kind<h2/>";
+}
